66e6d56d747dac9a4939169747adb8cf2262eca7
sara-1307 <saquil01@ucm.es>
2026-02-24 19:17:19 +0100

    Cámara
    Realice el scrip que hace que la cámara siga al jugador con cierto retraso, me quedo to boniko.

--- Assets/Scripts/Camera/Follow_Player.cs
+++ Assets/Scripts/Camera/Follow_Player.cs
@@ -49,50 +49,44 @@ public class Follow_Player : MonoBehaviour
         Quaternion lookAtRotation = Quaternion.LookRotation(dir, Vector3.up);
         transform.rotation = Quaternion.Lerp(transform.rotation, lookAtRotation, rotationspringFactor * Time.deltaTime);
 
-
-        #region Atributos del Inspector (serialized fields)
-        // Documentar cada atributo que aparece aquí.
-        // El convenio de nombres de Unity recomienda que los atributos
-        // públicos y de inspector se nombren en formato PascalCase
-        // (palabras con primera letra mayúscula, incluida la primera letra)
-        // Ejemplo: MaxHealthPoints
-
-        #endregion
-
-        // ---- ATRIBUTOS PRIVADOS ----
-        #region Atributos Privados (private fields)
-        // Documentar cada atributo que aparece aquí.
-        // El convenio de nombres de Unity recomienda que los atributos
-        // privados se nombren en formato _camelCase (comienza con _, 
-        // primera palabra en minúsculas y el resto con la 
-        // primera letra en mayúsculas)
-        // Ejemplo: _maxHealthPoints
-
-        #endregion
-
-        // ---- MÉTODOS DE MONOBEHAVIOUR ----
-        #region Métodos de MonoBehaviour
-
-        // Por defecto están los típicos (Update y Start) pero:
-        // - Hay que añadir todos los que sean necesarios
-        // - Hay que borrar los que no se usen 
-
-        /// <summary>
-        /// Start is called on the frame when a script is enabled just before 
-        /// any of the Update methods are called the first time.
-        /// </summary>
-        void Start()
-    {
-        
     }
+    #region Atributos del Inspector (serialized fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // públicos y de inspector se nombren en formato PascalCase
+    // (palabras con primera letra mayúscula, incluida la primera letra)
+    // Ejemplo: MaxHealthPoints
+
+    #endregion
+
+    // ---- ATRIBUTOS PRIVADOS ----
+    #region Atributos Privados (private fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // privados se nombren en formato _camelCase (comienza con _, 
+    // primera palabra en minúsculas y el resto con la 
+    // primera letra en mayúsculas)
+    // Ejemplo: _maxHealthPoints
+
+    #endregion
+
+    // ---- MÉTODOS DE MONOBEHAVIOUR ----
+    #region Métodos de MonoBehaviour
+
+    // Por defecto están los típicos (Update y Start) pero:
+    // - Hay que añadir todos los que sean necesarios
+    // - Hay que borrar los que no se usen 
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before 
+    /// any of the Update methods are called the first time.
+    /// </summary>
+   
 
     /// <summary>
     /// Update is called every frame, if the MonoBehaviour is enabled.
     /// </summary>
-    void Update()
-    {
-        
-    }
+    
     #endregion
 
     // ---- MÉTODOS PÚBLICOS ----
