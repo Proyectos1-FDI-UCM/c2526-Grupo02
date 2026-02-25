556706f33ce71bf39def8c1baa478ceb774fdad4
Alejandro Jiménez Rojo <alejji14@ucm.es>
2026-02-25 16:17:13 +0100

    añadida colisión de la cámara

--- Assets/Scripts/Camera/Follow_Player.cs
+++ Assets/Scripts/Camera/Follow_Player.cs
@@ -16,12 +16,17 @@ using UnityEngine;
 public class Follow_Player : MonoBehaviour
 {
     // ---- ATRIBUTOS DEL INSPECTOR ----
+    #region Atributos del Inspector (serialized fields)
     [SerializeField]
     private Transform Target;
     //[SerializeField]
     //private float TargetEyes = 0.5f;
     [SerializeField]
-    private float springFactor = 5f;
+    private float SpringFactor = 5f;
+    [SerializeField]
+    private float DistanceToWall = 8.5f;
+    [SerializeField]
+    private float rayHeight = 3f;
     //[SerializeField]
     //private float rotationspringFactor = 5f;
 
@@ -29,38 +34,6 @@ public class Follow_Player : MonoBehaviour
     //private Vector3 offset; //Distancia entre punto A y B. (El juagdor y la cámara)
     //private Vector3 currentPos; // Posicion actual con el movimiento 'suave' de la cámara
     //private Vector3 expectedPos;
-
-    void Start()
-    {
-        if (Target == null)
-        {
-            Debug.Log("No has asignado ningun target a la cámara");
-            return;
-        }
-    }
-    private void Update()
-    {
-
-        Vector3 playerAct = Target.transform.position;
-        Vector3 targetPos = transform.position;
-        //expectedPos = Target.position + offset; //Posicion ideal con el objetivo y el offset. 
-        //Vector3 act = transform.position;
-        ////Vector3 targetEyes = Target.position + Vector3.up * TargetEyes;
-        //Vector3 highPos = expectedPos + Vector3.up * 5f; 
-        targetPos.x = Mathf.Lerp(targetPos.x, playerAct.x, (springFactor * Time.deltaTime));
-        transform.position = targetPos;
-
-        /* Vector3 dir = (targetEyes - transform.position).normalized; *///Calculamos la dirección hacia los ojos
-                                                                         //Quaternion lookAtRotation = Quaternion.LookRotation(dir, Vector3.up);
-                                                                         //transform.rotation = Quaternion.Lerp(transform.rotation, lookAtRotation, rotationspringFactor * Time.deltaTime);
-    }
-    #region Atributos del Inspector (serialized fields)
-    // Documentar cada atributo que aparece aquí.
-    // El convenio de nombres de Unity recomienda que los atributos
-    // públicos y de inspector se nombren en formato PascalCase
-    // (palabras con primera letra mayúscula, incluida la primera letra)
-    // Ejemplo: MaxHealthPoints
-
     #endregion
 
     // ---- ATRIBUTOS PRIVADOS ----
@@ -80,16 +53,38 @@ public class Follow_Player : MonoBehaviour
     // Por defecto están los típicos (Update y Start) pero:
     // - Hay que añadir todos los que sean necesarios
     // - Hay que borrar los que no se usen 
+    void Start()
+    {
+        if (Target == null)
+        {
+            Debug.Log("No has asignado ningún target a la cámara");
+            return;
+        }
+    }
+    private void Update()
+    {
+        Vector3 playerAct = Target.transform.position;
+        Vector3 rayPos = Target.transform.position;
+        rayPos.y += rayHeight;
+        Vector3 targetPos = transform.position;
+        //expectedPos = Target.position + offset; //Posicion ideal con el objetivo y el offset. 
+        //Vector3 act = transform.position;
+        ////Vector3 targetEyes = Target.position + Vector3.up * TargetEyes;
+        //Vector3 highPos = expectedPos + Vector3.up * 5f; 
+        bool _collidingR = Physics2D.Raycast(rayPos, Vector2.right, DistanceToWall);
+        bool _collidingL = Physics2D.Raycast(rayPos, Vector2.left, DistanceToWall);
+        Debug.Log(_collidingL + "L " + _collidingR);
+        if (!_collidingR && !_collidingL)
+        {
+            targetPos.x = Mathf.Lerp(targetPos.x, playerAct.x, (SpringFactor * Time.deltaTime));
+            transform.position = targetPos;
+        }
+       
 
-    /// <summary>
-    /// Start is called on the frame when a script is enabled just before 
-    /// any of the Update methods are called the first time.
-    /// </summary>
-
-
-    /// <summary>
-    /// Update is called every frame, if the MonoBehaviour is enabled.
-    /// </summary>
+        /* Vector3 dir = (targetEyes - transform.position).normalized; *///Calculamos la dirección hacia los ojos
+                                                                         //Quaternion lookAtRotation = Quaternion.LookRotation(dir, Vector3.up);
+                                                                         //transform.rotation = Quaternion.Lerp(transform.rotation, lookAtRotation, rotationspringFactor * Time.deltaTime);
+    }
 
     #endregion
 
