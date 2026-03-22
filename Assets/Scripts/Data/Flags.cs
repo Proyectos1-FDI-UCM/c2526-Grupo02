2e4a2c3f81ee3a813afc8cf585cfa6a0de7e3cde
ppolegre <ppolegre@ucm.es>
2026-03-22 11:59:23 +0100

    Hechas las flags
    Falta concretar las condiciones pero eso se hace en grupo

---
+++ Assets/Scripts/Data/Flags.cs
@@ -0,0 +1,96 @@
+//---------------------------------------------------------
+// Breve descripción del contenido del archivo
+// Responsable de la creación de este archivo
+// Nombre del juego
+// Proyectos 1 - Curso 2025-26
+//---------------------------------------------------------
+
+using System.Diagnostics;
+using UnityEngine;
+// Añadir aquí el resto de directivas using
+
+
+/// <summary>
+/// ES un sitema que comprueba cuando se ha cumplido cirta condición y lo guarda
+/// Su utilida recide en que es como una especie de guardado de progreso para el juego 
+/// Es un array de bools, y luego un enum publico para darle nombres
+/// </summary>
+public class Flags : MonoBehaviour
+{
+
+    public enum Conditions { puzzle1, puzzle2, puzzle3}
+
+
+    // ---- ATRIBUTOS DEL INSPECTOR ----
+    #region Atributos del Inspector (serialized fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // públicos y de inspector se nombren en formato PascalCase
+    // (palabras con primera letra mayúscula, incluida la primera letra)
+    // Ejemplo: MaxHealthPoints
+
+    #endregion
+
+    // ---- ATRIBUTOS PRIVADOS ----
+    #region Atributos Privados (private fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // privados se nombren en formato _camelCase (comienza con _, 
+    // primera palabra en minúsculas y el resto con la 
+    // primera letra en mayúsculas)
+    // Ejemplo: _maxHealthPoints
+    private bool[] flags;
+
+    #endregion
+
+    // ---- MÉTODOS DE MONOBEHAVIOUR ----
+    #region Métodos de MonoBehaviour
+
+    // Por defecto están los típicos (Update y Start) pero:
+    // - Hay que añadir todos los que sean necesarios
+    // - Hay que borrar los que no se usen 
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before 
+    /// any of the Update methods are called the first time.
+    /// </summary>
+    void Start()
+    {
+        flags = new bool[Conditions.GetNames(typeof(Conditions)).Length];
+        UnityEngine.Debug.Log(flags.Length);
+    }
+    #endregion
+
+    // ---- MÉTODOS PÚBLICOS ----
+    #region Métodos públicos
+    // Documentar cada método que aparece aquí con ///<summary>
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+    // Ejemplo: GetPlayerController
+
+    public bool GetPos(Conditions n)
+    {
+        int i = (int) n;
+        return flags[i];
+    }
+
+    public void CambiaFlag(Conditions n, bool Cambio) 
+    {
+        int i = (int)n;
+        flags[i] = Cambio;
+    }
+
+    #endregion
+    
+    // ---- MÉTODOS PRIVADOS ----
+    #region Métodos Privados
+    // Documentar cada método que aparece aquí
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+
+    #endregion   
+
+} // class Flags 
+// namespace
